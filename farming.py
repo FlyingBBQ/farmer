050278f9aea6726149e29dbe0c227755a5d2460e
FlyingBBQ <derek.cardol@gmail.com>
2026-01-18 00:20:22 +0100

    Farm power with multiple drones

--- farming.py
+++ farming.py
@@ -14,9 +14,11 @@ def till_entity(entity):
         till()
 
 
-def grow(entity):
+def grow(entity, water_level=0):
     if can_harvest():
         harvest()
+    if get_water() <= water_level:
+        use_item(Items.Water)
     type = get_entity_type()
     if (type == None) or (type == Entities.Dead_Pumpkin) or (type == Entities.Grass):
         till_entity(entity)
@@ -142,24 +144,40 @@ def pumpkin():
     harvest()
 
 
+# Use a global to share state between spawn_drone()
+power_level = 15
 def power():
     util.goto(0, 0)
-    measured_power = {}
     world_size = get_world_size()
+
+    def plant_power():
+        plant_one_lane(grow, Entities.Sunflower, 0.6, North)
+
     for x in range(world_size):
-        for y in range(world_size):
-            grow(Entities.Sunflower)
-            measured_power[(get_pos_x(), get_pos_y())] = measure()
-            move(North)
+        spawn_drone(plant_power)
         move(East)
 
-    max_power = 15
-    while max_power >= 7:
-        for pos in measured_power:
-            if measured_power[pos] == max_power:
-                util.goto(pos[0], pos[1])
+    while num_drones() > 1:
+        pass
+
+    def harvest_power():
+        # power_level is 'read-only'
+        global power_level
+        for x in range(get_world_size()):
+            if power_level == measure():
+                while not can_harvest():
+                    pass
                 harvest()
-        max_power -= 1
+            move(North)
+
+    global power_level
+    power_level = 15
+    while power_level >= 7:
+        for x in range(world_size):
+            spawn_drone(harvest_power)
+            move(East)
+        # Decrement the global power level
+        power_level -= 1
 
 
 def cactus():
@@ -179,6 +197,12 @@ def cactus():
         move(East)
 
     harvest()
+
+
+def plant_one_lane(func, arg1, arg2, direction):
+    for x in range(get_world_size()):
+        func(arg1, arg2)
+        move(direction)
 
 
 def plant_one_field(func, args):
